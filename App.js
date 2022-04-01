ecaba7b6cd67bb459b982adb1d1fd651385a6a7b
Kevin Lai <kevinlai@hk01.com>
2022-04-01 10:37:36 +0800

    feat: use custom ScrollView for FlatList

--- App.js
+++ App.js
@@ -1,68 +1,140 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, SafeAreaView, View, FlatList } from 'react-native';
-import { useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  View,
+  FlatList,
+} from "react-native";
+import { useState, useCallback, useEffect, useRef } from "react";
+import CustomScrollView from "./CustomScrollView";
 
 const randomColor = () => {
   const color = Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, '0');
+    .padStart(6, "0");
   return `#${color}`;
 };
 
-const data = [...Array(20).keys()].map(i => ({
+const data = [...Array(60).keys()].map((i) => ({
   id: i,
-  text: `Item ${i}`
-}))
+  text: `Item ${i}`,
+}));
 
-const Item = ({ text, backgroundColor }) => (
-  <View style={[styles.item, { backgroundColor }]}>
-    <Text style={styles.text}>{text}</Text>
-  </View>
-);
+const Item = ({ text /*, backgroundColor, */, isSticky }) => {
+  const [ticker, setTicker] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(
+      () => setTicker((_ticker) => _ticker + 1),
+      1000
+    );
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <View style={[styles.item, isSticky && styles.itemSticky]}>
+      <Text style={styles.text}>
+        {text} ({ticker})
+      </Text>
+    </View>
+  );
+};
 
 const App = () => {
-  const [isSticky, setIsSticky] = useState(false)
+  const [stickyIndex, setStickyIndex] = useState(null);
 
-  const onButtonPress = () => {
-    setIsSticky((isSticky) => !isSticky)
-  }
+  const [color, setColor] = useState(randomColor());
 
-  const renderItem = backgroundColor => ({ item }) => (
-    <Item text={item.text} backgroundColor={backgroundColor}/>
+  const handleStickyIndex = (index) => {
+    setStickyIndex(index);
+    setColor(randomColor()); // reserved
+  };
+
+  const renderItem = useCallback(
+    ({ item, index /*, separators */ }) => {
+      return (
+        <Item
+          text={item.text}
+          // backgroundColor={color}
+          key={item.id}
+          isSticky={stickyIndex === index}
+        />
+      );
+    },
+    [stickyIndex]
   );
+
+  const keyExtractor = useCallback((item) => item.id, []);
+  const renderScrollComponent = useCallback(
+    (props) => (
+      <CustomScrollView
+        {...props}
+        stickyHeaderIndices={[
+          stickyIndex != null ? stickyIndex : Number.POSITIVE_INFINITY,
+        ]}
+      />
+    ),
+    [stickyIndex]
+  );
+
+  const flatListRef = useRef();
+
+  const isStickyActive = stickyIndex != null;
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Press below button to toggle Item 0 stickiness. Color change indicates a re-render.</Text>
-      <Button
-        onPress={onButtonPress}
-        title={isSticky ? 'Un-stick' : 'Stick'}
-      />
+      <View style={styles.topControl}>
+        <Button onPress={() => handleStickyIndex(null)} title="reset" />
+        <Text>|</Text>
+        <Button onPress={() => handleStickyIndex(0)} title="stick 0" />
+        <Text>|</Text>
+        <Button onPress={() => handleStickyIndex(1)} title="stick 1" />
+        <Text>|</Text>
+        <Button onPress={() => handleStickyIndex(5)} title="stick 5" />
+        <Text>|</Text>
+        <Button onPress={() => handleStickyIndex(9)} title="stick 9" />
+      </View>
       <FlatList
+        ref={flatListRef} // no use currently
         data={data}
-        renderItem={renderItem(randomColor())}
-        keyExtractor={item => item.id}
-        stickyHeaderIndices={isSticky ? [0] : []}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        automaticallyAdjustContentInsets={isStickyActive}
+        renderScrollComponent={renderScrollComponent} // XXX HACK we hack the default ScrollView component used in FlatList > VirtualizedList
       />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     marginHorizontal: 10,
   },
   item: {
-    backgroundColor: '#f9c2ff',
+    backgroundColor: "#dddddd",
     padding: 20,
     marginVertical: 8,
+  },
+  itemSticky: {
+    backgroundColor: "#ff0000",
+  },
+  topControl: {
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "row",
   },
   text: {
     fontSize: 20,
   },
 });
 
-export default App
+export default App;
